5c1416a398c385eab0a0f1eada69b6ac8d39e70e
Rafael Alfaro <rafaela@thehangar.cr>
2018-07-04 22:51:28 -0600

    Syncs players names

--- Assets/Scripts/SetupLocalPlayer.cs
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -5,6 +5,25 @@ using UnityEngine.Networking;
 
 public class SetupLocalPlayer : NetworkBehaviour {
 
+    [SyncVar]
+    public string pname = "player";
+
+	void OnGUI()
+	{
+        if (isLocalPlayer){
+            pname = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pname);
+            if(GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "Change")){
+                CmdChangeName(pname);
+            }
+        }
+	}
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pname = newName;
+    }
+
 	// Use this for initialization
 	void Start () {
         if(isLocalPlayer){
@@ -14,6 +33,8 @@ public class SetupLocalPlayer : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+           this.GetComponentInChildren<TextMesh>().text = pname;
 	}
+
+
 }
